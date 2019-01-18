453ce50bbe0e4c995e7ce51b8cde1a1dc09ff24a
SirPanda <hellpanda44@gmail.com>
2019-01-18 22:35:39 +0300

    Мигрировал БД и создал модели данных

---
+++ dTax/dTax/Models/DbPostrgreContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace dTax.Models
+{
+    public class DbPostrgreContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Role> Roles { get; set; }
+
+        public DbPostrgreContext(DbContextOptions<DbPostrgreContext> options) : base(options)
+        {
+            if (Database.EnsureCreated())
+                Initialization();
+
+        }
+
+        private void Initialization()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    
+
+}
--- dTax/dTax/Startup.cs
+++ dTax/dTax/Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.EntityFrameworkCore;
+using dTax.Models;
 
 namespace dTax
 {
@@ -24,6 +26,11 @@ namespace dTax
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
+            services.AddEntityFrameworkNpgsql()
+                .AddDbContext<DbPostrgreContext>(options =>
+                    options.UseNpgsql(Configuration.GetConnectionString("PostgreConnection")));
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
