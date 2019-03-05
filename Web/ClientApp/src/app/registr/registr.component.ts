b1eebcd5842a9c7e1cbd89bc9d55e6be4be026d6
PlushBeard1323 <46777047+PlushBeard1323@users.noreply.github.com>
2019-03-05 12:37:10 +0300

    регистрация без роли и времени

--- Web/ClientApp/src/app/registr/registr.component.ts
+++ Web/ClientApp/src/app/registr/registr.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpService } from '../Servise/http.service';
+import { Router } from '@angular/router';
+import {NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-registr',
@@ -7,9 +10,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistrComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route: Router, private http: HttpService) { }
 
   ngOnInit() {
+
+  }
+  reg(Form: NgForm) {
+    console.log(Form.value);
+    this.http.post('account/register', Form.value).subscribe(data => {
+      this.route.navigateByUrl('home');
+    }, err => {
+      alert('Хрень');
+    });
   }
 
 }
