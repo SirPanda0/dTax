89eb53a4ec9c1bd29962981a2cee44e56eda5fdb
PlushBeard1323 <46777047+PlushBeard1323@users.noreply.github.com>
2019-04-23 17:42:39 +0300

    авто
    выбор авто из бд

--- Web/ClientApp/src/app/avtoregistr/avtoregistr.component.ts
+++ Web/ClientApp/src/app/avtoregistr/avtoregistr.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpService } from '../Servise/http.service';
+import { Router } from '@angular/router';
+import {NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-avtoregistr',
@@ -6,10 +9,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./avtoregistr.component.css']
 })
 export class AvtoregistrComponent implements OnInit {
+CarBrand;
+  constructor(private route: Router, private http: HttpService) { }
 
-  constructor() { }
-
-  ngOnInit() {
+  ngOnInit() { 
+    this.Brand()
   }
-
-}
+  
+  Brand() {
+    this.http.get('CarBrand/Get').subscribe(data => {
+      this.CarBrand = data;
+    console.log(data)
+})
+}}
